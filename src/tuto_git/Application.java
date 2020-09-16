767cb544c409a7c94bfa80be60dbbb1ba3181652
ethanyang <chuunibyohan@gmail.com>
2020-09-16 12:26:15 +0200

    ajout d'un titre à la fenêtre

--- src/tuto_git/Application.java
+++ src/tuto_git/Application.java
@@ -27,7 +27,7 @@ public class Application extends Frame implements WindowListener {
 		
 		this.add(view);
 		this.pack();
-		this.setTitle("The clic game");
+		this.setTitle("wenhan");
 		this.setVisible(true);	
 	}
 
